24d419479940b7b9e08a6ada13d6e2a1fcaf0f2e
joeyholland15 <joeyholland15@gmail.com>
2015-11-18 15:40:36 -0800

    Completed functional pattern for stacks and queues.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = {};
+  stackInstance.size = 0; 
+  stackInstance.storage = {}; 
+  return stackInstance; 
 };
 
 var stackMethods = {};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
-
+  var queueSize = 0; 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var first = 1; 
+  var last = 1;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
+    queueSize = last - first; 
   };
 
   someInstance.dequeue = function() {
+
+    if(queueSize > 0) {
+      queueSize = last - first; 
+      var holder = storage[first]; 
+      delete storage[first];
+      first++;
+      queueSize = last - first; 
+      return holder; 
+    }
   };
 
   someInstance.size = function() {
+    return last - first; 
   };
 
   return someInstance;
