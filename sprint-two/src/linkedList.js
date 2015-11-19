17def848bddf0a8aea905c752902a70a6bf84f8b
joeyholland15 <joeyholland15@gmail.com>
2015-11-19 10:08:56 -0800

    Completed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value); 
+    if(list.tail) {
+      list.tail.next = newNode; 
+    }
+    list.tail = newNode; 
+    if(list.head === null) {
+      list.head = newNode; 
+    }
   };
 
   list.removeHead = function() {
+    var holder = list.head.value;
+    list.head = list.head.next; 
+    return holder; 
   };
 
   list.contains = function(target) {
+    for(var key in list) {
+      if(list[key].value === target) {
+        return true; 
+      }
+    };
+    return false; 
   };
 
   return list;
