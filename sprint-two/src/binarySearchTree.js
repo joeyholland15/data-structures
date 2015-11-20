d744984f1c8c3e67e9916eb7cb2caaacd6a45477
joeyholland15 <joeyholland15@gmail.com>
2015-11-19 20:44:21 -0800

    Completed through binarySearchTrees

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,62 @@
 var BinarySearchTree = function(value) {
+  var newSearchTree = Object.create(binarySearchMethods);
+  newSearchTree.left = null;
+  newSearchTree.right = null;
+  newSearchTree.value = value;
+  return newSearchTree;
 };
 
+binarySearchMethods = {};
+
+binarySearchMethods.insert = function(newValue) {
+  function findLocation(node) {
+    if(newValue < node.value && node.left === null) {
+      node.left = BinarySearchTree(newValue);
+    } else if(newValue > node.value && node.right === null) {
+      node.right = BinarySearchTree(newValue); 
+    } else if(newValue < node.value && node.left !== null) {
+      findLocation(node.left);
+    } else if(newValue > node.value && node.right !== null) {
+      findLocation(node.right);
+    }
+  };
+  findLocation(this); 
+}
+
+
+
+binarySearchMethods.contains = function(newValue) {
+  var status = false; 
+  function searchNodes(node) { 
+    if(node.value === newValue) {
+      status = true;
+      return;  
+    }
+    if(node.left === null && node.right === null) {
+      return;
+    }
+    if(newValue < node.value) {
+      searchNodes(node.left)
+    } else {
+      searchNodes(node.right)
+    }
+  }
+  searchNodes(this); 
+  return status;
+}
+
+binarySearchMethods.depthFirstLog = function(callback) {
+  function execute(node) {
+    callback(node.value);
+    if(node.left !== null) {
+      execute(node.left)
+    }
+    if(node.right !== null) {
+      execute(node.right); 
+    }
+  }
+  execute(this); 
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
