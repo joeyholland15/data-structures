7c5e56d9881e049f389f34a32e705e4e45eac867
joeyholland15 <joeyholland15@gmail.com>
2015-11-19 16:40:53 -0800

    Updated hash table add method

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,30 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit); 
+  var obj = {};
+  obj[k] = v;
+  this._storage.set(index, obj); 
+
+  //if(this_.storage.get(index) === undefined) {
+//     var array = [];
+//     array.push({k:v});
+//   } else {
+//     this._storage.get(index).push({k:v}); 
+//   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index); 
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var returnVal = this._storage.get(index);
+  this._storage.set(index, undefined); 
+  return returnVal;
 };
 
 
