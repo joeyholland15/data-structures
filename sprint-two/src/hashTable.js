3c4e275a60a73bce8f6fb574b9116426886d0eeb
joeyholland15 <joeyholland15@gmail.com>
2015-11-20 11:04:45 -0800

    Updated hash tables to use arrays instead of objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,11 +8,18 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit); 
   if(this._storage.get(index) === undefined) {
-    var obj = {};
-    obj[k] = v;
-    this._storage.set(index, obj); 
+    var bucket = [];
+    bucket.push([k,v]);
+    this._storage.set(index, bucket); 
   } else {
-    this._storage.get(index)[k] = v; 
+    var bucket = this._storage.get(index);
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] === k) {
+        bucket[i][1] = v;
+      } else {
+        bucket.push([k,v]); 
+      }
+    } 
   }
 
   //if(this_.storage.get(index) === undefined) {
@@ -25,15 +32,28 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k]; 
-
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(k === bucket[i][0]) {
+      return bucket[i][1]; 
+    };
+  }
 };
 
 HashTable.prototype.remove = function(k) {
+  //var index = getIndexBelowMaxForKey(k, this._limit);
+  //var returnVal = this._storage.get(index)[k];
+  //delete this._storage.get(index)[k];  
+  //return returnVal;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var returnVal = this._storage.get(index)[k];
-  delete this._storage.get(index)[k];  
-  return returnVal;
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(k === bucket[i][0]) {
+      var holder = bucket[i][1]; 
+      delete bucket[i][1]; 
+      return holder; 
+    };
+  }
 };
 
 
